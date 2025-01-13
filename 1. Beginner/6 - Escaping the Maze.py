e4193c5bb92c3e786acf916cea75cd583131c5a6
Mayconsmy <1mayconsm2@gmail.com>
2025-01-13 19:31:50 -0300

    Day 6

--- 1. Beginner/6 - Escaping the Maze.py
+++ 1. Beginner/6 - Escaping the Maze.py
@@ -1,1 +1,14 @@
-print("a")
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
