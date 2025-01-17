77dcba1f9ce46b11d4e28c29081c4b0f0c6b35a3
Mayconsmy <1mayconsm2@gmail.com>
2025-01-17 19:23:08 -0300

    commits

--- 1. Beginner/08 - caesar cipher.py
+++ 1. Beginner/08 - caesar cipher.py
@@ -1,6 +1,6 @@
-import art
+import art_8
 
-print(art.logo)
+print(art_8.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
