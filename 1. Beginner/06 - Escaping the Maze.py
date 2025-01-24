eda68525e04ce60662e5dd2747ee1a86d0cb193c
Mayconsmy <1mayconsm2@gmail.com>
2025-01-24 20:44:32 -0300

    commits

--- 1. Beginner/06 - Escaping the Maze.py
+++ 1. Beginner/06 - Escaping the Maze.py
@@ -1,15 +1,15 @@
 #I created a game in which a robot programmed in python must escape from a random maze
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+#def turn_right():
+#    turn_left()
+#    turn_left()
+#    turn_left()
 
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clear():
-        move()
-    else:
-        turn_left()
+#while not at_goal():
+#    if right_is_clear():
+#        turn_right()
+#        move()
+#    elif front_is_clear():
+#        move()
+#    else:
+#        turn_left()
 
